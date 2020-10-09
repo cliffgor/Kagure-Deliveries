4f6bfe4eff3bc907bb00c19ddc0af69d231d9836
cliffgor <clierozigy@gmail.com>
2020-10-09 16:22:47 +0300

    Remove logo import preventing netlify to render the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Welcome to - Kagure Deliveries
+        Welcome to Kagure Deliveries
       </h1>
     </div>
   );
